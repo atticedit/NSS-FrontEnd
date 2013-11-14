ecd8083297d5f1d455cc82e1148d234a70885223
Lars Soderkvist <updates11@mac.com>
2013-11-14 13:38:39 -0600

    Add basic attack feature.

--- 2013-11-13-Socket-IO-Multiplayer-Game/public/javascripts/app/app.js
+++ 2013-11-13-Socket-IO-Multiplayer-Game/public/javascripts/app/app.js
@@ -1,25 +1,80 @@
-/* global document, getValue, window, io */
+/* global getValue, document, window, io */
 
 $(document).ready(initialize);
 
-var socket;
-var player;
-var color;
-var name;
+var socket, game, player, color, players = [];
 
 function initialize(){
   $(document).foundation();
   initializeSocketIO();
   $('#start').on('click', clickStart);
+  $('body').on('keyup', keyupMove);
+}
+
+// ------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------- //
+
+function keyupMove(e){
+  var isArrow = _.any([37, 38, 39, 40], function(i){return i === e.keyCode;});
+
+  if(e.keyCode === 68){
+    // d key
+    var prey = findPrey();
+    socket.emit('attack', {game:game, predator:player, prey:prey.name});
+  }
+
+  if(isArrow){
+    var p = findPredator();
+
+    switch(e.keyCode){
+      case 38:
+        // down arrow key
+        p.y--;
+        break;
+      case 40:
+        // up arrow key
+        p.y++;
+        break;
+      case 37:
+        // left arrow key
+        p.x--;
+        break;
+      case 39:
+        // right arrow key
+        p.x++;
+        break;
+    }
+
+    socket.emit('playermoved', {game:game, player:player, x:p.x, y:p.y});
+  }
 }
 
 function clickStart(){
-  name = getValue('#name');
+  game = getValue('#game');
   player = getValue('#player');
   color = getValue('#color');
-  $('#game').removeClass('hidden');
-  socket.emit('startgame', {name: name, player: player, color: color});
+  $('table#game').removeClass('hidden');
+  $('#current-player').css('color', color).text(':: ' + player + ' ::');
+  socket.emit('startgame', {game:game, player:player, color:color});
 }
+
+// ------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------- //
+
+function findPredator(){
+  return _.find(players, function(p){return p.name === player;});
+}
+
+function findPrey(){
+  var predator = findPredator();
+  return _.find(players, function(p){return p.x === predator.x && p.y === predator.y && p.name !== player;});
+}
+
+// ------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------- //
 
 function initializeSocketIO(){
   var port = window.location.port ? window.location.port : '80';
@@ -27,8 +82,42 @@ function initializeSocketIO(){
 
   socket = io.connect(url);
   socket.on('connected', socketConnected);
+  socket.on('playerjoined', socketPlayerJoined);
 }
 
 function socketConnected(data){
   console.log(data);
 }
+
+function socketPlayerJoined(data){
+  players = data.players;
+  htmlResetBoard();
+
+  for(var i = 0; i < data.players.length; i++){
+    if(data.players[i].health > 0){
+      htmlAddPlayer(data.players[i]);
+    }
+  }
+}
+
+// ------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------- //
+
+function htmlResetBoard(){
+  $('.cell .health').css('background-color', '#4a84a1');
+  $('.cell .player').css('background-color', '#4a84a1');
+  $('.cell .player').text('');
+}
+
+function htmlAddPlayer(player){
+  var $cell = $('.cell[data-x="' + player.x + '"][data-y="' + player.y + '"]');
+  $cell.find('.health').css('background-color', '#293e5a');
+  $cell.find('.health').css('width', player.health + '%');
+  $cell.find('.player').css('background-color', player.color);
+  $cell.find('.player').text(player.name);
+}
+
+// ------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------- //
+// ------------------------------------------------------------------------- //
--- 2013-11-13-Socket-IO-Multiplayer-Game/sockets/common.js
+++ 2013-11-13-Socket-IO-Multiplayer-Game/sockets/common.js
@@ -1,44 +1,58 @@
-var mongoose = require('mongoose');
-var Game = mongoose.model('Game');
-var Player = mongoose.model('Player');
+var async = require('async');
+var __ = require('lodash');
+var m = require('../lib/mechanics');
+var io;
 
 exports.connection = function(socket){
+  io = this;
   socket.emit('connected', {status: 'connected'});
   socket.on('disconnect', socketDisconnect);
   socket.on('startgame', socketStartGame);
+  socket.on('playermoved', socketPlayerMoved);
+  socket.on('attack', socketAttack);
 };
 
-function socketDisconnect(){
+function socketStartGame(data){
+  var storage = {};
+  var socket = this;
+
+  async.waterfall([
+    function(fn){m.findGame(data.game,fn);},
+    function(game,fn){if(!game){m.newGame(data.game,fn);}else{fn(null,game);}},
+    function(game,fn){storage.game=game;fn();},
+    function(fn){m.findPlayer(data.player,fn);},
+    function(player,fn){if(!player){m.newPlayer(data.player,data.color,fn);}else{fn(null,player);}},
+    function(player,fn){m.resetPlayer(player,socket,fn);},
+    function(player,fn){storage.player=player;fn();},
+    function(fn){fn(null,__.any(storage.game.players,function(p){return p.id===storage.player.id;}));},
+    function(isFound,fn){if(!isFound){m.attachPlayer(storage.game,storage.player,fn);}else{fn(null,storage.game);}},
+    function(game,fn){m.findGame(data.game,fn);},
+    function(game,fn){m.emitPlayers(io.sockets,game.players,fn);}
+  ]);
 }
 
-function socketStartGame(data){
-  console.log('received a start game message from a browser');
+function socketPlayerMoved(data){
   console.log(data);
 
-  Game.findOne({name: data.name}, function(err, game){
-    if(game){
-      console.log('found old game');
-      console.log(game);
-      addPlayer(game, data);
-    } else {
-      new Game({name: data.name}).save(function(err, game){
-        console.log('created new game');
-        console.log(game);
-        addPlayer(game, data);
-      });
-    }
-  });
+  async.waterfall([
+    function(fn){m.findPlayer(data.player,fn);},
+    function(player,fn){m.updateCoordinates(player,data.x,data.y,fn);},
+    function(player,fn){m.findGame(data.game,fn);},
+    function(game,fn){m.emitPlayers(io.sockets,game.players,fn);}
+  ]);
 }
 
-function addPlayer(game, data){
-  new Player({name: data.player, color: data.color}).save(function(err, player){
-    console.log('player created');
-    console.log(player);
+function socketAttack(data){
+  console.log(data);
 
-    player.health = 100;
-    player.save(function(err, player){
+  async.waterfall([
+    function(fn){m.findPlayer(data.prey,fn);},
+    function(player,fn){m.playerAttacked(player,fn);},
+    function(player,fn){m.findGame(data.game,fn);},
+    function(game,fn){m.emitPlayers(io.sockets,game.players,fn);}
+  ]);
+}
 
-    });
-
-  });
+function socketDisconnect(data){
+  console.log(data);
 }
